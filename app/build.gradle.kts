f87051e42008a55423c91b43ba9a4b2ea80ec4cc
demin6632-del <demin6632@gmail.com>
2026-09-26 03:28:40 +0500

    Add persistent WebSocket transport

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,4 +13,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.mlkit:text-recognition:16.0.1")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
